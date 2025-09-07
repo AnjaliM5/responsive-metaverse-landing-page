87ff55db0c1d4f6075dfd96924ed44be05a1935c
AnjaliM5 <ajmitra05@gmail.com>
2025-09-08 01:23:56 +0530

    bug fixes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -20,24 +20,71 @@ window.addEventListener('scroll', () => {
     window.addEventListener('scroll', handleScroll);
 });
 
+// navbar - scroll - to - bottom effect
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.scroll-nav-animate');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled-bottom');
+    } else {
+        navbar.classList.remove('scrolled-bottom');
+    }
+});
 
-// const container = document.querySelector('.spiral-container');
-// const centerX = 900;
-// const centerY = 200;
-// const totalNodes = 5;
 
-// for (let i = 0; i < totalNodes; i++) {
-//     const angle = i * 50; // Spread nodes evenly around circle
-//     const radius = 100 + i * 8; // Gradually increase radius for spiral effect
 
-//     const x = centerX + radius * Math.cos(angle * Math.PI / 180);
-//     const y = centerY + radius * Math.sin(angle * Math.PI / 180);
+// overall screen floating elements animation 
+const maxElements = 5;
 
-//     const node = document.createElement('div');
-//     node.classList.add('plan-node');
-//     node.style.left = `${x}px`;
-//     node.style.top = `${y}px`;
-//     node.innerHTML = `<span>Plan ${i + 1}<br>2.${i}%</span>`;
+function createMarble() {
+    if (document.querySelectorAll('.marble').length >= maxElements) {
+        return;
+    }
+    const marble = document.createElement('img');
+    marble.src = './assets/marble.png';
+    marble.classList.add('marble');
 
-//     container.appendChild(node);
-// }
+    const size = Math.random() * 60;
+
+    marble.style.width = `${size}px`;
+    marble.style.height = `${size}px`;
+
+    marble.style.left = `${Math.random() * 100}vw`;
+
+    const duration = Math.random() * 20 + 10;
+    marble.style.animationDuration = `${duration}s`;
+
+    document.body.appendChild(marble);
+
+    marble.addEventListener('animationend', () => {
+        marble.remove();
+    });
+}
+
+setInterval(createMarble, 1500);
+
+function createGeometry() {
+    if (document.querySelectorAll('.geometry').length >= maxElements) {
+        return;
+    }
+    const geometry = document.createElement('img');
+    geometry.src = './assets/geometric-shape.png';
+    geometry.classList.add('geometry');
+
+    const size = Math.random() * 60;
+
+    geometry.style.width = `${size}px`;
+    geometry.style.height = `${size}px`;
+
+    geometry.style.left = `${Math.random() * 100}vw`;
+
+    const duration = Math.random() * 20 + 10;
+    geometry.style.animationDuration = `${duration}s`;
+
+    document.body.appendChild(geometry);
+
+    geometry.addEventListener('animationend', () => {
+        geometry.remove();
+    });
+}
+
+setInterval(createGeometry, 1500);
