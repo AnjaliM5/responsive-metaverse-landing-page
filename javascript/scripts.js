319481f2a92f4006eb6f830b4741a222a42b23d1
AnjaliM5 <ajmitra05@gmail.com>
2025-09-07 12:24:27 +0530

    Completed About Us section layout and styling

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,21 @@
+// hero section floating cards fadeout on load
 const floatingContainer = document.querySelector('.floating-cards-container');
 window.addEventListener('load', () => {
     floatingContainer.classList.add('fadeOut');
+});
+
+// slidein effect for title
+const titleSlideIn = document.querySelector('.title-slide-in');
+window.addEventListener('scroll', () => {
+    function handleScroll() {
+        const rect = titleSlideIn.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (rect.top < windowHeight - 100) {
+            titleSlideIn.classList.add('slide-in');
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
 });
